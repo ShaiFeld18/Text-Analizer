80f5e0ef9df72b9028ddcd07bd89555ab478731b
Shai Feldman <shai.feldman@mail.huji.ac.il>
2025-01-29 10:38:24 +0200

    fix path search

--- src/text_analyzer/graph.py
+++ src/text_analyzer/graph.py
@@ -1,37 +1,53 @@
-from typing import List
+from typing import List, Set
 
 
 class GraphAnalyzer:
-    """Handles graph operations"""
+    """Handles graph operations for finding paths"""
 
     @staticmethod
-    def find_all_paths(graph: List[List[List[str]]],
-                       start: List[str],
+    def find_all_paths(graph: List[List[str]],
+                       start: str,
                        end: str,
-                       max_len: int) -> List[List[List[str]]]:
+                       max_len: int) -> List[List[str]]:
         """
-        Find all paths between two nodes in a graph with maximum length.
-        :param graph: graph of connected people.
-        :param start: starting node.
-        :param end: destination node.
-        :param max_len: max length of a path.
-        :return: List of all possible paths from start to end up to max length.
+        Find all paths between two nodes in an undirected graph with maximum length.
+
+        :param graph: List of edges where each edge is a list of two node names.
+        :param start: Starting node name.
+        :param end: Destination node name.
+        :param max_len: Maximum allowed length of a path.
+        :return: List of all possible paths from start to end
         """
-        paths = []
-        stack = [(start, [start])]  # Stack holds tuples of (current_node, current_path)
 
-        while stack:
-            current, path = stack.pop()
-
-            if len(path) > max_len:
-                continue
-            if current == end:
-                paths.append(path)
-                continue
-
+        def get_neighbors(node: str) -> List[str]:
+            """Find all neighbors of a given node in the graph."""
+            neighbors = []
             for edge in graph:
-                if (edge[0] == current and edge[1] not in path) or (edge[1] == current and edge[0] not in path):
-                    next_node = edge[1] if edge[0] == current else edge[0]
-                    stack.append((next_node, path + [next_node]))
+                if edge[0] == node:
+                    neighbors.append(edge[1])
+                elif edge[1] == node:
+                    neighbors.append(edge[0])
+            return neighbors
+
+        def dfs(current: str, path: List[str], paths: List[List[str]], visited: Set[str]) -> None:
+            """recursive function for dfs."""
+            if len(path) > max_len:
+                return
+
+            if current == end:
+                paths.append(path[:])
+                return
+
+            for neighbor in get_neighbors(current):
+                if neighbor not in visited:
+                    visited.add(neighbor)
+                    # find all paths from this node
+                    dfs(neighbor, path + [neighbor], paths, visited)
+                    # because we want all paths and not just one
+                    visited.remove(neighbor)
+
+        paths: List[List[str]] = []
+        visited: Set[str] = {start}
+        dfs(start, [start], paths, visited)
 
         return paths
